bef9d547bfec55e00daf743136a6c17bf25de75d
rpiotrow <rpiotrow@gmail.com>
2023-05-20 19:51:05 +0200

    init

---
+++ src/main/java/rpiotrowski/billtracker/BillTrackerApplication.java
@@ -0,0 +1,13 @@
+package rpiotrowski.billtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillTrackerApplication.class, args);
+    }
+
+}
